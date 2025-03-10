8e780a7e049573b241f6b0f79bba6fa215d869ee
delta <60275031+deltaseconds@users.noreply.github.com>
2025-03-09 19:59:08 -0700

    logout

--- app/account/login/page.js
+++ app/account/login/page.js
@@ -7,11 +7,13 @@ import Link from 'next/link';
 export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
     const token = localStorage.getItem('token');
     if (token) {
+      setIsLoggedIn(true);
       router.replace('/account/profile');
     }
   }, []);
@@ -41,47 +43,51 @@ export default function Login() {
     <div className="register-form">
       <title>VENTURIZE - Login</title>
       <h1>LOGIN</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="email">E-MAIL ADDRESS</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="password">PASSWORD</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit" className="animated-button">
-          <svg viewBox="0 0 24 24" className="arr-2" xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
-            ></path>
-          </svg>
-          <span className="text">LOGIN</span>
-          <span className="circle"></span>
-          <svg viewBox="0 0 24 24" className="arr-1" xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
-            ></path>
-          </svg>
-        </button>
-      </form>
-      <p>
-        <Link className="redirect" href="/account/register">
-          Don't have an account?
-        </Link>
-      </p>
+      {!isLoggedIn && (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="email">E-MAIL ADDRESS</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="password">PASSWORD</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit" className="animated-button">
+            <svg viewBox="0 0 24 24" className="arr-2" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
+              ></path>
+            </svg>
+            <span className="text">LOGIN</span>
+            <span className="circle"></span>
+            <svg viewBox="0 0 24 24" className="arr-1" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
+              ></path>
+            </svg>
+          </button>
+        </form>
+      )}
+      {!isLoggedIn && (
+        <p>
+          <Link className="redirect" href="/account/register">
+            Don't have an account?
+          </Link>
+        </p>
+      )}
     </div>
   );
 }
--- app/account/register/page.js
+++ app/account/register/page.js
@@ -1,6 +1,7 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 
 export default function Register() {
@@ -8,6 +9,16 @@ export default function Register() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [repeatpassword, setRepeatPassword] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      setIsLoggedIn(true);
+      router.replace('/account/profile');
+    }
+  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -15,7 +26,7 @@ export default function Register() {
       alert('Passwords do not match!');
       return;
     }
-  
+
     const response = await fetch('/api/register', {
       method: 'POST',
       headers: {
@@ -35,70 +46,73 @@ export default function Register() {
 
   return (
     <div className="register-form">
-        <title>VENTURIZE - Register</title>
+      <title>VENTURIZE - Register</title>
       <h1>REGISTER</h1>
-      <form onSubmit={handleSubmit} >
-        <div>
-          <label htmlFor="username">USERNAME</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="email">E-MAIL ADDRESS</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="password">PASSWORD</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="password">REPEAT PASSWORD</label>
-          <input
-            type="password"
-            id="repeatpassword"
-            value={repeatpassword}
-            onChange={(e) => setRepeatPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit" className="animated-button">
-          <svg viewBox="0 0 24 24" className="arr-2" xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
-            ></path>
-          </svg>
-          <span className="text">REGISTER ACCOUNT</span>
-          <span className="circle"></span>
-          <svg viewBox="0 0 24 24" className="arr-1" xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
-            ></path>
-          </svg>
-        </button>
-
-      </form>
-      <p>
-        <Link className="redirect" href="/account/login">
-          Have an account already?
-        </Link>
-      </p>
+      {!isLoggedIn && (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="username">USERNAME</label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="email">E-MAIL ADDRESS</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="password">PASSWORD</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="repeatpassword">REPEAT PASSWORD</label>
+            <input
+              type="password"
+              id="repeatpassword"
+              value={repeatpassword}
+              onChange={(e) => setRepeatPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit" className="animated-button">
+            <svg viewBox="0 0 24 24" className="arr-2" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
+              ></path>
+            </svg>
+            <span className="text">REGISTER</span>
+            <span className="circle"></span>
+            <svg viewBox="0 0 24 24" className="arr-1" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
+              ></path>
+            </svg>
+          </button>
+        </form>
+      )}
+      {!isLoggedIn && (
+        <p>
+          <Link className="redirect" href="/account/login">
+            Already have an account? Login here.
+          </Link>
+        </p>
+      )}
     </div>
   );
 }
